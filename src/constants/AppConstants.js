c2ca5ee3fdc9b790c458a281900fe1027ce6ac2f
Ayeni Olusegun <nsegun5@gmail.com>
2016-09-02 23:09:18 +0100

    fix bugs

--- src/constants/AppConstants.js
+++ src/constants/AppConstants.js
@@ -7,5 +7,5 @@ module.exports = {
     PASSWORD : 'sy6KJsT8',
     ENTITY_ID : '8a8294174b7ecb28014b9699220015ca',
     NEXT_PAYMENT_IN_MIN: 60,
-    PAYON_REDIRECT : 'http://iamraphson.github.io',
+    PAYON_REDIRECT : 'http://microapp-donation.herokuapp.com',
 }
